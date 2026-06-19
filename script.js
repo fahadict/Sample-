47b4815f21ec69de8cab283df3b6628e6cc87c2b
fahadict <jamalfahadhm@gmail.com>
2026-06-19 17:04:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function toggleMenu() {
-    document.getElementById('mobileMenu').classList.toggle('show');
+// FIXED MOBILE MENU BUG
+function toggleMenu(){
+  document.getElementById("menu").classList.toggle("show");
 }
+
+// SIMPLE SCROLL ANIMATION
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(e=>{
+    if(e.isIntersecting){
+      e.target.classList.add("show-animation");
+    }
+  });
+});
+
+document.querySelectorAll("section").forEach(el=>{
+  observer.observe(el);
+});
